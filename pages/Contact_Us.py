6d7c480ec59ab8226e5795489f45cc20c10a0a4c
chuka <emry390@gmail.com>
2025-05-12 16:03:18 +0100

    Fixed logical error preventing subject of email in message body from reflecting in email.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -5,12 +5,12 @@ st.header("Contact Me")
 
 with st.form(key="email_forms", clear_on_submit=True):
     user_email = st.text_input("Your email address")
-    user_input = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
     message = f"""\
-    Subject: New email from Portfolio App!\
-    
-    From: {user_email}
-    {user_input}
+Subject: New email from Portfolio App!
+
+From: {user_email}
+{raw_message}
     """
     button = st.form_submit_button("Submit")
     if button:
